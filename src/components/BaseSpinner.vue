628fd96d6589eefd53fc29615d8e7fd0a644894c
giteshk11 <writemail2gitesh@gmail.com>
2020-10-12 16:02:58 +0530

    added spinner and redirection to introdution

---
+++ src/components/BaseSpinner.vue
@@ -0,0 +1,42 @@
+<template functional>
+  <div
+    aria-label="Loading"
+    class="loading"
+    :class="[props.classNames, props.borderColor]"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSpinner',
+  props: {
+    borderColor: {
+      type: String,
+      default: 'border-white'
+    },
+    classNames: String
+  }
+};
+</script>
+
+<style scoped>
+.spinner-image {
+  animation: spin 1.2s infinite ease-in-out;
+}
+
+.loading {
+  @apply border-4 rounded-full w-10 h-10;
+  animation: spin 500ms linear infinite;
+  border-bottom-color: transparent;
+  border-right-color: transparent;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
