fd81047e77c34cfac66dc5126ee94d411ba7ee16
giteshk11 <writemail2gitesh@gmail.com>
2020-10-12 10:49:00 +0530

    added firebase auth. #issue-1

--- src/components/auth/AuthButton.vue
+++ src/components/auth/AuthButton.vue
@@ -16,9 +16,10 @@
 </template>
 
 <script>
-import { mapMutations, mapGetters } from 'vuex';
 import Button from '@/components/inputs/Button.vue';
 // import facebookLogo from '@/assets/facebook2.png';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 export default {
   name: 'LinkAccount',
@@ -30,25 +31,33 @@ export default {
       socialMediaProviders: [
         {
           name: 'facebook'
-        },
-
-        {
-          name: 'google'
         }
       ]
     };
   },
-  computed: {
-    ...mapGetters('auth', {
-      oauthLink: 'oauth'
-    })
+  created() {
+    firebase
+      .auth()
+      .getRedirectResult()
+      .then(function(result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          console.log(token);
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   },
   methods: {
-    ...mapMutations('auth', ['updateProvider']),
-    async oauth(provider) {
-      await this.updateProvider(provider);
-      const redirectUrl = await this.oauthLink;
-      window.location.assign(redirectUrl);
+    async oauth() {
+      var provider = new firebase.auth.FacebookAuthProvider();
+      firebase.auth().signInWithRedirect(provider);
     }
   }
 };
